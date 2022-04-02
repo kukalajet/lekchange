967eb4b41d8db60f9bbf6f0e46576309f2025c51
Jeton Kukalaj <jeton.kukalaj@crispybacon.it>
2022-04-02 16:00:00 +0200

    refactor `modal_content`

--- lib/widgets/modal_content.dart
+++ lib/widgets/modal_content.dart
@@ -12,8 +12,68 @@ class ModalContent extends StatelessWidget {
       builder: (context, state) {
         final value = state.value;
 
-        return Text(value, maxLines: 3);
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 8.0),
+            Header(
+              onDismiss: () {
+                Navigator.of(context).pop();
+                context.read<ScanBloc>().add(const ScanValueDismissed());
+              },
+            ),
+            Body(value: value),
+            const SizedBox(height: 24.0),
+          ],
+        );
       },
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key, required this.onDismiss}) : super(key: key);
+
+  final Function() onDismiss;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(),
+        ElevatedButton(
+          onPressed: onDismiss,
+          child: const Icon(
+            Icons.close_sharp,
+            color: Colors.black54,
+            size: 24,
+          ),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.grey[300],
+            shape: const CircleBorder(),
+            padding: const EdgeInsets.all(8.0),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({Key? key, required this.value}) : super(key: key);
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Text(
+        value,
+        maxLines: 3,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
