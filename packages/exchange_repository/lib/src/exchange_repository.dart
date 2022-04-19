6e0c9ad9a45f1f6ba83da8c42c677af638268b84
Jeton Kukalaj <jeton.kukalaj@crispybacon.it>
2022-04-19 21:25:28 +0200

    update mock

--- packages/exchange_repository/lib/src/exchange_repository.dart
+++ packages/exchange_repository/lib/src/exchange_repository.dart
@@ -84,29 +84,185 @@ class ExchangeRepository {
 
 const mock = '''
 [
-  {
-    "name": "Euro",
-    "code": "EUR",
-    "symbol": "€",
-    "rate": 0.0081
-  },
-  {
-    "name": "U.S. Dollar",
-    "code": "USD",
-    "symbol": "\$",
-    "rate": 0.0091
-  },
-  {
-    "name": "Pound Sterling",
-    "code": "GBP",
-    "symbol": "£",
-    "rate": 0.0069
-  },
-  {
-    "name": "Russian Ruble",
-    "code": "RUB",
-    "symbol": "₽",
-    "rate": 0.072
-  }
+   {
+      "code":"EUR",
+      "rate":0.0083,
+      "name":"Euro",
+      "symbol":"€"
+   },
+   {
+      "code":"USD",
+      "rate":0.0090,
+      "name":"U.S. Dollar",
+      "symbol":"\$"
+   },
+   {
+      "code":"CHF",
+      "rate":0.0085,
+      "name":"Swiss Franc",
+      "symbol":"CHf"
+   },
+   {
+      "code":"GBP",
+      "rate":0.0069,
+      "name":"British Pound",
+      "symbol":"£"
+   },
+   {
+      "code":"DKK",
+      "rate":0.0632,
+      "name":"Danish Krone",
+      "symbol":"Kr."
+   },
+   {
+      "code":"NOK",
+      "rate":0.0812,
+      "name":"Norwegian Krone",
+      "symbol":"kr"
+   },
+   {
+      "code":"SEK",
+      "rate":0.0883,
+      "name":"Swedish Krona",
+      "symbol":"kr"
+   },
+   {
+      "code":"CAD",
+      "rate":0.0114,
+      "name":"Canadian Dollar",
+      "symbol":"CA\$"
+   },
+   {
+      "code":"AUD",
+      "rate":0.0123,
+      "name":"Australian Dollar",
+      "symbol":"AU\$"
+   },
+   {
+      "code":"JPY",
+      "rate":1.3699,
+      "name":"Japanese Yen",
+      "symbol":"¥"
+   },
+   {
+      "code":"TRY",
+      "rate":0.1412,
+      "name":"Turkish Lira",
+      "symbol":"₺"
+   },
+   {
+      "code":"MKD",
+      "rate":0.5405,
+      "name":"Macedonian Denar",
+      "symbol":"Ден"
+   },
+   {
+      "code":"BGN",
+      "rate":0.0191,
+      "name":"undefined",
+      "symbol":"undefined"
+   },
+   {
+      "code":"RON",
+      "rate":0.0483,
+      "name":"Romanian Leu",
+      "symbol":"lei"
+   },
+   {
+      "code":"HUF",
+      "rate":3.7037,
+      "name":"Hungarian Forint",
+      "symbol":"Ft"
+   },
+   {
+      "code":"RUB",
+      "rate":0.8475,
+      "name":"Russian Ruble",
+      "symbol":"₽"
+   },
+   {
+      "code":"PLN",
+      "rate":0.0455,
+      "name":"Polish Złoty",
+      "symbol":"zł"
+   },
+   {
+      "code":"HRK",
+      "rate":0.0698,
+      "name":"Croatian Kuna",
+      "symbol":"kn"
+   },
+   {
+      "code":"CZK",
+      "rate":0.2375,
+      "name":"Czech Koruna",
+      "symbol":"Kč"
+   },
+   {
+      "code":"ILS",
+      "rate":0.0361,
+      "name":"Israeli Shekel",
+      "symbol":"₪"
+   },
+   {
+      "code":"CNY",
+      "rate":0.0706,
+      "name":"Renminbi",
+      "symbol":"CN¥"
+   },
+   {
+      "code":"RSD",
+      "rate":1.1494,
+      "name":"Serbian Dinar",
+      "symbol":"din"
+   },
+   {
+      "code":"ISK",
+      "rate":1.5625,
+      "name":"Icelandic Króna",
+      "symbol":"kr"
+   },
+   {
+      "code":"MXN",
+      "rate":0.2304,
+      "name":"Mexican Peso",
+      "symbol":"MX\$"
+   },
+   {
+      "code":"AED",
+      "rate":0.0383,
+      "name":"United Arab Emirates Dirham",
+      "symbol":"د.إ"
+   },
+   {
+      "code":"SAR",
+      "rate":0.0442,
+      "name":"Saudi Riyal",
+      "symbol":"ر.س"
+   },
+   {
+      "code":"SGN",
+      "rate":0.0160,
+      "name":"Singapore Dollar",
+      "symbol":"S\$"
+   },
+   {
+      "code":"KWD",
+      "rate":0.0036,
+      "name":"Kuwaiti Dinar",
+      "symbol":"د.ك"
+   },
+   {
+      "code":"QAR",
+      "rate":0.0429,
+      "name":"Qatari Riyal",
+      "symbol":"ر.ق"
+   },
+   {
+      "code":"BHD",
+      "rate":0.0044,
+      "name":"Bahraini Dinar",
+      "symbol":"د.ب"
+   }
 ]
 ''';
